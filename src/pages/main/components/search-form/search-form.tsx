10be2027ff1ec2c7e36dd388c0dade82b5eb2d4a
ваше имя <dimarik-tixonov@yandex.ru>
2022-10-24 19:57:47 +0300

    add: добавит логику для формы поиска

--- src/pages/main/components/search-form/search-form.tsx
+++ src/pages/main/components/search-form/search-form.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import cn from 'classnames';
 import styles from './search-form.module.scss';
 import { ReactComponent as SearchIcon } from './search.svg';
 import { ReactComponent as FilterIcon } from './filter.svg';
 import { ReactComponent as ResetIcon } from './reset.svg';
+import SearchFilter from '../search-filter/search-filter';
+
+const initialState = '';
 
 function SearchForm(): JSX.Element {
+  const [searchState, setSearchState] = useState(initialState);
+  const [isSearching, setIsSearching] = useState<boolean>(Boolean(searchState));
+  const [openFilter, setOpenFilter] = useState(false);
+
+  const handleSearch = ({ target }: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = target;
+
+    setSearchState(value);
+  };
+
+  const resetSearchForm = (): void => {
+    setSearchState(initialState);
+  };
+
+  useEffect(() => {
+    setIsSearching(Boolean(searchState));
+  }, [searchState]);
+
   return (
     <div className={styles.searchForm}>
       <div className={styles.searchIconWrapper}>
@@ -15,10 +36,12 @@ function SearchForm(): JSX.Element {
           height="19px"
         />
       </div>
+      {isSearching && (
       <div className={styles.searchControls}>
         <button
           className={cn(styles.searchButton, styles.searchButtonParams)}
           type="button"
+          onClick={() => setOpenFilter(!openFilter)}
         >
           <span className={styles.searchButtonText}>Фильтры</span>
           <FilterIcon
@@ -36,18 +59,23 @@ function SearchForm(): JSX.Element {
             className={styles.resetIcon}
             width="24px"
             height="24px"
+            onClick={resetSearchForm}
           />
         </button>
       </div>
+      )}
       <form autoComplete="off">
         <input
           className={styles.searchInput}
-          type="search"
+          type="text"
           autoCorrect="off"
           autoCapitalize="off"
           placeholder="Поиск видео"
+          onChange={handleSearch}
+          value={searchState}
         />
       </form>
+      {openFilter && <SearchFilter />}
     </div>
   );
 }
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import SearchFilter from './components/search-filter/search-filter';
 import SearchForm from './components/search-form/search-form';
 
 function Main(): JSX.Element {
   return (
-    <>
-      <SearchForm />
-      <SearchFilter />
-    </>
+    <SearchForm />
   );
 }
 
