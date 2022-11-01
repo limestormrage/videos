592d7ca83b545b0fbcbfe63fc25a3d9f978d0ed8
ваше имя <dimarik-tixonov@yandex.ru>
2022-11-01 19:22:47 +0300

    add: добавит компонент видео-листа

---
+++ src/pages/main/components/video-list/video-list.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './video-list.module.scss';
+
+function VideoList(): JSX.Element {
+  return (
+    <section className={styles.videoList}>
+      <ul>
+        <li>
+          a
+        </li>
+        <li>
+          a
+        </li>
+        <li>
+          a
+        </li>
+        <li>
+          a
+        </li>
+        <li>
+          a
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default VideoList;
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import SearchForm from './components/search-form/search-form';
+import VideoList from './components/video-list/video-list';
 
 function Main(): JSX.Element {
   return (
-    <SearchForm />
+    <>
+      <SearchForm />
+      <VideoList />
+    </>
   );
 }
 
